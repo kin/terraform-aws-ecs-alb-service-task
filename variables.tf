b5e61febbb37eaf81966c0a4e246e47a1be5b6cd
Monica Hart <monica.hart@kin-mb-6846.lan>
2024-11-22 10:34:26 -0600

    updating to add rift check in locals

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "ecs_load_balancers" {
     elb_name         = string
     target_group_arn = string
   }))
-  default = []
+  default     = []
   description = "A list of load balancer config objects for the ECS service; see [ecs_service#load_balancer](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service#load_balancer) docs"
 }
 
