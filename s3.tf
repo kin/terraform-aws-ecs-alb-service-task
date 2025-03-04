153de7e9bcdbb8749657de59e8b87146f4a11fd2
Monica Hart <monica.hart@kin.com>
2025-03-04 11:24:40 -0600

    updating

--- s3.tf
+++ s3.tf
@@ -2,15 +2,26 @@
 
 resource "local_file" "appspec" {
   count    = var.deployment_controller_type == "CODE_DEPLOY" ? 1 : 0
+
   content  = local.appspec_content
   filename = "${path.module}/appspec.yml"
 }
 
-data "archive_file" "appspec" {
-  count       = var.deployment_controller_type == "CODE_DEPLOY" ? 1 : 0
-  type        = "zip"
-  source_file = local_file.appspec[0].filename
-  output_path = "${path.module}/appspec.zip"
+resource "null_resource" "zip_appspec" {
+  count = var.deployment_controller_type == "CODE_DEPLOY" ? 1 : 0
+
+  triggers = {
+    appspec_content_hash = local.appspec_sha256
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      cd ${path.module}
+      zip appspec.zip appspec.yml
+    EOT
+  }
+
+  depends_on = [local_file.appspec]
 }
 
 resource "aws_s3_bucket" "appspec_artifacts" {
@@ -34,11 +45,11 @@ resource "aws_s3_object" "appspec_artifacts" {
 
   bucket  = aws_s3_bucket.appspec_artifacts[0].id
   key     = "source/appspec.zip"  # Pre-zipped file
-  source  = data.archive_file.appspec[0].output_path
-  etag    = filemd5(data.archive_file.appspec[0].output_path)
+  content = file("${path.module}/appspec.zip")
+  etag    = local.appspec_sha256
   tags    = module.this.tags
 
-  depends_on = [data.archive_file.appspec]
+  depends_on = [null_resource.zip_appspec]
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "appspec_artifacts" {
