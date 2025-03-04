151e13f5b1e218b7df1f034121c2c7c69085929e
Monica Hart <monica.hart@kin.com>
2025-03-04 17:14:55 -0600

    using the exec arn

--- codepipeline.tf
+++ codepipeline.tf
@@ -63,10 +63,8 @@ resource "aws_iam_role_policy" "default" {
       },
       {
         Effect = "Allow"
-        Action = "iam:PassRole"
-        Resource = [
-          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.container_name}"
-        ]
+        Action = ["iam:PassRole"]
+        Resource = "${aws_iam_role.ecs_exec.arn}"
       }
     ]
   })
