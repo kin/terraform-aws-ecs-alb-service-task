523cd7f408a14d37e86ccf354e44c7df299c7869
C.Thomas Clark <c.thomas.clark.pdx@gmail.com>
2026-01-03 20:07:54 -0800

    Terinery operators are hard

--- main.tf
+++ main.tf
@@ -428,7 +428,7 @@ data "aws_subnet" "private_subnet_cidr" {
 #this should allow traffic from ecs services in the cluster to the web app for service connect
 
 resource "aws_security_group_rule" "allow_ingress_form_cluster_services" {
-  count                    = local.create_security_group && length(var.service_connect_configurations[0].service) == 0  ? 1 : 0
+  count                    = local.create_security_group && length(var.service_connect_configurations[0].service) != 0  ? 1 : 0
   description              = "Allow ingress from cluster services security groups"
   type                     = "ingress"
   from_port                = var.container_port
